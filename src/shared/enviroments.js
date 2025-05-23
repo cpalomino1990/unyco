ecb5cafc0ac9eccd8cf391736eb1b9f94b5c152d
cpalomino1990 <89701110+cpalomino1990@users.noreply.github.com>
2025-05-23 10:54:00 -0500

    sdfdf

--- src/shared/enviroments.js
+++ src/shared/enviroments.js
@@ -1,2 +1,2 @@
-  export const host = "https://merry-palmier-ba5f3e.netlify.app/dist/widget.js";
+  export const host = "https://unico01.netlify.app";
 // export const host = "http://127.0.0.1:5501"; // Production
