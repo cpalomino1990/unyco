ed1056f90c59d6e8b4f7ca292188f6debd75a507
cpalomino1990 <89701110+cpalomino1990@users.noreply.github.com>
2025-05-23 10:45:53 -0500

    asdfsdfgdfg

--- src/shared/constants/enviroments.js
+++ src/shared/constants/enviroments.js
@@ -1,5 +1,5 @@
 // export const host = "https://merry-palmier-ba5f3e.netlify.app";
-export const host = "http://127.0.0.1:5502";
+export const host = "https://unico01.netlify.app";
 export const apiHost = "http://134.209.218.43:3000";
 
 // Detectar si es disposito movil
